111e5f4e36910ac6bfb413acc6dc5596dd083ed2
javierlete <jglete@gmail.com>
2018-03-26 15:52:51 +0200

    Entidades

---
+++ Entidades/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public int IdDepartamento { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public Decimal Sueldo { get; set; }
+        public string Dni { get; set; }
+
+        public Departamento DepartamentoAsignado { get; set; }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nick { get; set; }
+        public string Password { get; set; }
+        public int IdEmpleado { get; set; }
+
+        public Empleado EmpleadoAsociado { get; set; }
+    }
+}
